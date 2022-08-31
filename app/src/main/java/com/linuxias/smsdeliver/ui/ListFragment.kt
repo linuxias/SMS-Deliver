4e1da7d0479c7f9da8dc3739856c92331860da04
Seungha Son <linuxias@gmail.com>
2022-09-01 00:28:49 +0900

    Add floatingActionButton to ListFragment
     This button is to start dialog fragment what user
     add filter information.
    
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- app/src/main/java/com/linuxias/smsdeliver/ui/ListFragment.kt
+++ app/src/main/java/com/linuxias/smsdeliver/ui/ListFragment.kt
@@ -6,16 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.linuxias.smsdeliver.R
+import com.linuxias.smsdeliver.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.filterAddBtn.setOnClickListener {
+            // Todo: Start dialog fragment to set filter information
+        }
     }
 }
