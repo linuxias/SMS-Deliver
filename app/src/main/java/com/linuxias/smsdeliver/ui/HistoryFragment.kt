4bc4720555edaa5a995132587565da26a5ba2fe3
Seungha Son <linuxias@gmail.com>
2022-09-05 06:25:49 +0900

    Adjust viewBinding to Fragments
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- app/src/main/java/com/linuxias/smsdeliver/ui/HistoryFragment.kt
+++ app/src/main/java/com/linuxias/smsdeliver/ui/HistoryFragment.kt
@@ -6,17 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.linuxias.smsdeliver.R
+import com.linuxias.smsdeliver.databinding.FragmentHistoryBinding
+import com.linuxias.smsdeliver.databinding.FragmentListBinding
 
 class HistoryFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/linuxias/smsdeliver/ui/MoreSettingFragment.kt
+++ app/src/main/java/com/linuxias/smsdeliver/ui/MoreSettingFragment.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.linuxias.smsdeliver.R
+import com.linuxias.smsdeliver.databinding.FragmentListBinding
+import com.linuxias.smsdeliver.databinding.FragmentMoreSettingBinding
 
 class MoreSettingFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentMoreSettingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_more_setting, container, false)
+        _binding = FragmentMoreSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
