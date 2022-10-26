1731ac90bb735fd3569a8bf24c0567ca61950877
Seungha Son <linuxias@gmail.com>
2022-10-26 23:29:00 +0900

    Add fakeRepository to test viewModel
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- app/src/test/java/com/linuxias/smsdeliver/repository/FakeFilterRepository.kt
+++ app/src/test/java/com/linuxias/smsdeliver/repository/FakeFilterRepository.kt
@@ -1,7 +1,21 @@
 package com.linuxias.smsdeliver.repository
 
-import com.linuxias.smsdeliver.data.FilterRepositoryImpl
+import com.linuxias.smsdeliver.data.FilterEntity
+import com.linuxias.smsdeliver.data.FilterRepository
 
-class FakeFilterRepository: FilterRepositoryImpl {
+class FakeFilterRepository: FilterRepository {
+    private val filters = mutableListOf<FilterEntity>()
+
+    override suspend fun insert(filterEntity: FilterEntity) {
+        filters.add(filterEntity)
+    }
+
+    override fun getAllFilters(): List<FilterEntity> {
+        return filters
+    }
+
+    override suspend fun deleteFilter(filterEntity: FilterEntity) {
+        filters.remove(filterEntity)
+    }
 
 }
